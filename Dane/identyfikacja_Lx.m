1f7e54677aa64b008c719fc99e065239b1d110f1
Piotr Palucki <piotrek.palucki@gmail.com>
2016-11-21 00:15:53 +0100

    Updated report, part 1

--- Dane/identyfikacja_Lx.m
+++ Dane/identyfikacja_Lx.m
@@ -1,30 +1,4 @@
-clear all;
-close all;
-
-% load('identtfikacja_skok_pradu_z_pwm_tuy.mat');
-% t = t(:, 1) - t(1,1); %przesuniecie wykresu do czasu 0
-% y = y(:, 1) - y(1,1); %przesuniecie do 0
-% 
-% %Transmitancja modelu - np. obiekt 1 rzedu
-% K = y(end, 1);      %dobrane doświadczalnie, nie ma znaczenia w tym przypadku
-% T = 0.048;          %w pracy[1] wyszło 6.3 ms
-% 
-% NUM = K;
-% DEN = [T 1];
-% 
-% model = tf(NUM, DEN);
-% skok = step(model, t);
-% 
-% u = K*u; %Dla lepszego zobrazowania
-% 
-% %Kryterium jakości aproksymacji
-% e = y - skok;
-% error = sum(e.^2)
-% 
-% figure
-% hold on;
-% plot(t, u, t, y, t, skok);
-
+clear all; close all;
 load identyfikacja_indukcyjnosc
 
 
@@ -32,7 +6,7 @@ load identyfikacja_indukcyjnosc
 %Linear regression for y = ax + b -> Y = AB
 Y = prad_A;
 A = [x_mm ones(length(x_mm), 1)];
-%B = [a b];
+% will be B = [a b];
 B = A\Y;
 a = B(1) 
 b = B(2)
@@ -45,7 +19,7 @@ y = a*x+b;
 figure
 hold on;
 plot(x_mm, prad_A, '*', x, y);
-title('Zaleznosc prądu cewki od położenia sfery');
+title('Zależność prądu cewki od położenia sfery');
 xlabel('x [mm]');
 ylabel('I [A]');
 legend('Dane pomiarowe','Dopasowanie metodą najmniejszych kwadratów');
@@ -65,6 +39,6 @@ plot(x_mm, -Lprim_dosw, '*', x, -Lprim);
 title('Pochodna indukcyjności w funkcji położenia sfery');
 xlabel('x [mm]');
 ylabel('L''(x) [H/m] ');
-legend('Dane pomiarowe','Dopasowanie');
+legend('Dane pomiarowe','Dopasowanie metodą najmniejszych kwadratów');
 
 
