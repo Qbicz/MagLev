70cb929388e95afd0e1c7fa592f1104c57923866
Piotr Palucki <piotrek.palucki@gmail.com>
2016-11-28 21:21:13 +0100

    Dodalem modele/dane z 28.11

--- Modele/model_nielin_zmienne.m
+++ Modele/model_nielin_zmienne.m
@@ -1,23 +1,23 @@
-%Autorzy: Filip Kubicz, Piotr Pa�ucki
+%Autorzy: Filip Kubicz, Piotr Pa?ucki
 %Opis: 
-%Skrypt inicjalizuj�cy zmienne wykorzystywane w modelu nieliniowym.
-%Model nielniowy zbudowany w celu weryfikacji wynik�w identyfikacji
+%Skrypt inicjalizuj?cy zmienne wykorzystywane w modelu nieliniowym.
+%Model nielniowy zbudowany w celu weryfikacji wynik?w identyfikacji
 
 m = 58;         %g
 g = 9.81;       %m/s^2
-u_c = -0.0062;  %V - stale napicie na cewce     
+u_c = -0.0062;  %V - stale napicie na cewce
 T = 0.024;      %s - stala czasowa cewki
 k = 0.2607;     %wspolczynnik wzmocnienia cewki
 a = 0.0928;     %parametry cewki
 b = 0.0214;     %parametry cewki
-%a = a+b;
-%b = a-b
-%a = a-b
+a = a+b;
+b = a-b
+a = a-b
 
-% Warunki pocz�tkowe
+Warunki pocz?tkowe
 x10 = 0.010; % m, pozycja sfery
 x20 = -1e-10;  %m/s, predkosc sfery
-x30 = 1e-10;  %A, prad cewki
+x30 = 1e-9;  %A, prad cewki
 
 % wartosc zadana
 x1zad = 0.012 % [m]
@@ -27,13 +27,14 @@ x1zad = 0.012 % [m]
 
 % regulator LQR dla zlinearyzowanego modelu
 x1stab = 0.014  % [m]
-x3stab = 0.023; % [A]
+x3stab = 1; % [A]
+%x3stab = 0.023; % [A]
 A21 = 0.002*a*g*x3stab^2/(a*x1stab+b)^3
 A23 = -0.002*g*x3stab/(a*x1stab+b)^2
 
 Q = diag([10 1 1])
 R = 1
-L = 2.46 % wzmocnienie wartosci zadanej
+L = 10 % wzmocnienie wartosci zadanej
 % L = 2.6 dla 14 mm
 % L = 2.73 dla 16mm
 % L = 2.46 dla 12 mm - wszystko dla linearyzacji w [0.014 0 0.023]
@@ -48,4 +49,4 @@ ST = ctrb(A, B)
 
 [K,S,E] = lqr(A,B,Q,R)
 
-sim('nieliniowy')
+%sim('nieliniowy')
---
+++ Modele/model_nieliniowy_zmienne.m.old
@@ -0,0 +1,20 @@
+%Autorzy: Filip Kubicz, Piotr Pa³ucki
+%Opis: 
+%Skrypt inicjalizuj¹cy zmienne wykorzystywane w modelu nieliniowym.
+%Model nielniowy zbudowany w celu weryfikacji wyników identyfikacji
+
+m = 58;         %g
+g = 9.81;       %m/s^2
+u_c = -0.0062;   %V - stale napicie na cewce     
+T = 0.024;      %s - stala czasowa cewki
+k = 0.2607;     %wspolczynnik wzmocnienia cewki
+a = 0.0928;     %parametry cewki
+b = 0.0214;     %parametry cewki
+a = a+b;
+b = a-b
+a = a-b
+
+% Warunki pocz¹tkowe
+x10 = 0.014; % m, pozycja sfery
+x20 = 0.0;  %m/s, predkosc sfery
+x30 = 1.38;  %A, prad cewki
